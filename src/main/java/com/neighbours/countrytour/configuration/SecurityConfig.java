a46db599fb440d88c45ab77648a97189ef6ee216
BojidarSh <bojidar.shumanov@gmail.com>
2020-03-26 20:59:41 +0200

    Remove unnecessary commented code
    Removed unnecessary commented code.

--- src/main/java/com/neighbours/countrytour/configuration/SecurityConfig.java
+++ src/main/java/com/neighbours/countrytour/configuration/SecurityConfig.java
@@ -13,26 +13,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(a -> a
-                        .antMatchers("/", "/error", "/webjars/**", "/api/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .logout(l -> l
-                        .logoutSuccessUrl("/").permitAll()
-                )
-                .csrf(c -> c
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-                )
-                .oauth2Login();
-    }*/
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
